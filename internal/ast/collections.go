37346b0bfb708d1535af3672c9917f24a53db923
SK MOHAMMAD ALI <alizonecontacts@gmail.com>
2026-08-25 15:11:31 +0530

    feat(ast): add collection and member expression nodes

---
+++ internal/ast/collections.go
@@ -0,0 +1,39 @@
+package ast
+
+import "github.com/skmohammadali786/gogo/internal/source"
+
+type ArrayExpr struct {
+	Span  source.Span
+	Items []Expr
+}
+func (ArrayExpr) node() {}
+func (ArrayExpr) expr() {}
+
+type ObjectProperty struct {
+	Span  source.Span
+	Key   string
+	Value Expr
+}
+
+type ObjectExpr struct {
+	Span       source.Span
+	Properties []ObjectProperty
+}
+func (ObjectExpr) node() {}
+func (ObjectExpr) expr() {}
+
+type MemberExpr struct {
+	Span   source.Span
+	Object Expr
+	Name   string
+}
+func (MemberExpr) node() {}
+func (MemberExpr) expr() {}
+
+type IndexExpr struct {
+	Span  source.Span
+	Object Expr
+	Index Expr
+}
+func (IndexExpr) node() {}
+func (IndexExpr) expr() {}
