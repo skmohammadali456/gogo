6d09ea5349bbe259e6e3e6c52441133212e2f47e
SK MOHAMMAD ALI <alizonecontacts@gmail.com>
2026-08-25 15:20:50 +0530

    test(ast): verify Step 3 span and optional invariants

--- internal/ast/collections_test.go
+++ internal/ast/collections_test.go
@@ -10,9 +10,14 @@ func TestCollectionNodesExposeSpans(t *testing.T) {
 	span := source.Span{Start: source.Position{Offset: 0, Line: 1, Column: 1}, End: source.Position{Offset: 3, Line: 1, Column: 4}}
 	array := ArrayExpr{Span: span}
 	object := ObjectExpr{Span: span}
-	member := MemberExpr{Span: span}
+	member := MemberExpr{Span: span, Optional: true}
 	index := IndexExpr{Span: span}
-	if SpanOf(array) != span || SpanOf(object) != span || SpanOf(member) != span || SpanOf(index) != span {
-		t.Fatal("collection AST nodes must preserve their spans")
+	assignment := AssignmentExpr{Span: span}
+	conditional := ConditionalExpr{Span: span}
+	if SpanOf(array) != span || SpanOf(object) != span || SpanOf(member) != span || SpanOf(index) != span || SpanOf(assignment) != span || SpanOf(conditional) != span {
+		t.Fatal("AST nodes must preserve their spans")
+	}
+	if !member.Optional {
+		t.Fatal("member AST must preserve optional access")
 	}
 }
